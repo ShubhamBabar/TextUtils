683d05376003d5a6b1a1cdb36059cc69d745bf6e
Shubham babar <121241828+ShubhamBabar@users.noreply.github.com>
2024-07-09 00:07:24 +0530

    navbar link disable

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,14 +11,14 @@ export default function Navbar(props) { // This is mendatory props should pass a
     <>
     <nav className={`navbar navbar-expand-lg bg-${props.mode}`} data-bs-theme={`${props.mode}`}>
   <div className="container-fluid">
-  <a className="navbar-brand" href="/">{props.name}</a>  {/* This is how props is defined in component */}
+  <a className="navbar-brand" href="#">{props.name}</a>  {/* This is how props is defined in component */}
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+          <a className="nav-link active" aria-current="page" href="#">{props.home}</a>
         </li> 
         {/* <li className="nav-item">
           <a className="nav-link"  href="/about">{props.aboutus}</a>
