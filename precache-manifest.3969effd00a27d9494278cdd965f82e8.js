ef5ef8ed91bd0816bb2df03dbc274cad8adaf765
Shubham babar <121241828+ShubhamBabar@users.noreply.github.com>
2024-07-08 23:57:14 +0530

    Updates

---
+++ precache-manifest.3969effd00a27d9494278cdd965f82e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac2b10aa1073e8baa208ebb044e3f4f",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "4b4d1bbd6ba78865fc86",
+    "url": "/TextUtils/static/css/main.f4a5512d.chunk.css"
+  },
+  {
+    "revision": "0b3860014b35efb2633b",
+    "url": "/TextUtils/static/js/2.77ec8cdb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils/static/js/2.77ec8cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f14db9e6497bb054d74",
+    "url": "/TextUtils/static/js/3.2763cf7e.chunk.js"
+  },
+  {
+    "revision": "4b4d1bbd6ba78865fc86",
+    "url": "/TextUtils/static/js/main.aba44800.chunk.js"
+  },
+  {
+    "revision": "f8ee5bdd8fdc6d867b3e",
+    "url": "/TextUtils/static/js/runtime-main.688bc8e2.js"
+  }
+]);
